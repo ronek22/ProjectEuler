1cff1c362bf2ca8f9e832648fd535e517fa1430f
Jakub Ronkiewicz <jronek3010@gmail.com>
2017-07-05 14:10:45 +0200

    Update 006.py

--- Python/006.py
+++ Python/006.py
@@ -1,3 +1,5 @@
+# Brute force solution
+"""
 sum = 0
 sq = 0
 for i in range(1,101):
@@ -7,3 +9,9 @@ for i in range(1,101):
 sq = sq**2
 
 print sq - sum
+"""
+# More more efficient solution involve algebra
+n = 100
+suma = n*(n+1)/2
+sum_sq = (2 * n + 1)*(n + 1)*n/6
+print suma**2 - sum_sq
