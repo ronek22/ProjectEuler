47b76e3a10031476d38fd1657eb69a8a65c06c16
Jakub Ronkiewicz <jronek3010@gmail.com>
2017-07-09 11:40:56 +0200

    Made faster algorithm

--- Python/004.py
+++ Python/004.py
@@ -1,3 +1,5 @@
+import time
+
 def isPalindrome(number):
 	x = str(number)
 	n = len(x)
@@ -6,15 +8,47 @@ def isPalindrome(number):
 		if x[i] != x[n-i-1]:
 			return False
 		i += 1
-	
-	return True
-	
-wynik = []
 
-for x in range(100,1000):
-	for y in range(100,1000):
-		test = x * y
-		if isPalindrome(test):
-			wynik.append(test)
-			
-print max(wynik)
+	return True
+
+def PalindromTest1():
+	wynik = []
+
+	for x in range(100,1000):
+		for y in range(100,1000):
+			test = x * y
+			if isPalindrome(test):
+				wynik.append(test)
+
+	print max(wynik)
+
+def PalindromTest2():
+	wynik = 0
+	for x in range(1000,100,-1):
+		if x*x < wynik:
+			break
+		for y in range(1000,100,-1):
+			test = x*y
+			if isPalindrome(test):
+				if(test > wynik):
+					wynik = test
+	print wynik
+
+print "#1 Algorytm"
+print '-'*15
+start_time=time.time()
+PalindromTest1()
+total_time = time.time() - start_time
+print "CZAS: ", total_time
+
+print '\n'
+
+print "#2 Algorytm"
+print '-'*15
+start_time=time.time()
+PalindromTest2()
+total_time2 = time.time() - start_time
+print "CZAS: ", total_time2
+
+quicker = (total_time-total_time2)/total_time*100
+print "\nAlgorytm #2 szybszy o %d%%" % quicker
