ce91e2d98e671d61d3b48fb77c08cb4a9721a73b
Jakub Ronkiewicz <jronek3010@gmail.com>
2017-08-11 17:39:10 +0200

    012 done

---
+++ Python/012.py
@@ -0,0 +1,40 @@
+from math import sqrt
+# too slow for n > 200
+def simpleCountDivisors(n):
+    # listDiv = []
+    numOfDiv = 0
+    for i in range(1,n+1):
+        if(n%i==0):
+            # listDiv.append(i)
+            numOfDiv+=1
+    # numOfDiv = len(listDiv)
+    # print str(n)+": ",
+    # for d in listDiv:
+        # print d,
+    return numOfDiv
+
+def countDivisors(n):
+    nod = 0 # number of divisors
+    nSqrt = int(sqrt(n))
+
+    for i in range(1,nSqrt+1):
+        if(n%i==0):
+            nod+=2
+
+    if (nSqrt*nSqrt == n):
+        nod-=1
+
+    return nod
+
+
+def test(x):
+    n=0
+    number = 0
+    i=1
+    while countDivisors(number) <= x:
+        number+=i
+        i+=1
+    return number
+
+
+print test(500)
