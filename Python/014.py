a76f3276a5a9c6e2dd91dec9faa900b20ed399d5
Jakub Ronkiewicz <jronek3010@gmail.com>
2017-08-16 12:44:57 +0200

    Now it's works much faster[70" improve to 3"]

--- Python/014.py
+++ Python/014.py
@@ -9,21 +9,7 @@ def collatz_terms_rec(n,count=0):
         count+=1
         return collatz_terms(3*n+1,count)
 
-def collatz_terms2(n):
-    count = 0
-    while n>1:
-        if n in visited_collatz.keys():
-            count += visited_collatz[n]
-            break;
-        elif(n%2==0):
-            count+=1
-            n = n/2
-        else:
-            count+=1
-            n = 3*n+1
-    return count
-
-def collatz_terms(n):
+def collatz_terms_slower(n):
     count = 0
     while n>1:
         if(n%2==0):
@@ -34,20 +20,32 @@ def collatz_terms(n):
             n = 3*n+1
     return count
 
+def collatz_terms(n):
+    count = 0
+    while n>1:
+        if n in visited:
+            count += visited[n]
+            n=1
+        elif(n%2==0):
+            count+=1
+            n = n/2
+        else:
+            count+=1
+            n = 3*n+1
+    return count
 
 
-number = 1
+number = 3
 max_number = 0
 max_terms = 0
-# visited_collatz = {}
+visited = {1:1,2:2}
 
 while number < 1000000:
     terms = collatz_terms(number)
-    # visited_collatz[number] = terms
-    # print "%d\t%d" % (number,terms)
+    visited[number] = terms
     if(terms > max_terms):
         max_terms = terms
         max_number = number
-    number+=1
+    number+=2 # because odd numbers always have biggest chains
 
 print "Max number is %d and that sequence have %d terms" % (max_number, max_terms)
