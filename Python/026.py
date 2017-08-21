a81dd3c3e1eb3bcf67fc3b3719ff6f5113810416
Jakub Ronkiewicz <jronek3010@gmail.com>
2017-08-22 00:48:09 +0200

    added condition cycle==i-1 end; now for 10^7 loop solve in 17"

--- Python/026.py
+++ Python/026.py
@@ -3,6 +3,8 @@ import os
 import time
 from itertools import count,islice
 
+MAX = 1000
+
 def isPrime(n):
 	return n>1 and all(n%i for i in islice(count(2), int(sqrt(n)-1)))
 
@@ -15,7 +17,7 @@ def generatePrime(n):
     return primes
 
 def solutionPrimes():
-    p = generatePrime(1000)
+    p = generatePrime(MAX)
     maks_i = 0
     maks = 0
     for i in p:
@@ -23,35 +25,37 @@ def solutionPrimes():
         n=0
         while x!=1:
             n+=1
-            x=pow(10,n,i) # this is faster
-            # x = (10**n)%i
+            x=pow(10,n,i)
         if(n>maks):
             maks=n
             maks_i = i
+        if(n==i-1):
+            break
         # print "1/%d cycle = %d" % (i,n)
-
     print maks_i
 
 def solutionByHand():
     max_cycle=0
-    for i in range(1000,1,-1):
-        # print i
+    for i in range(MAX,1,-1):
         remainders = set()
         remainders.add(1)
-        cycle = 1
         rem = 10%i
         while rem not in remainders:
             remainders.add(rem)
             if rem==0:
-                cycle = 0
+                remainders.clear()
                 break
             rem = (rem*10)%i
-            cycle+=1
+        cycle = len(remainders)
         if cycle>max_cycle:
             max_cycle = cycle
             number = i
-        # print "1/%d cycle = %d" % (i,cycle)
+        #cycle length < i
+        if cycle==i-1:
+            break
     print number
+
+
 
 def timeCount(func):
     start_time = time.time()
@@ -60,7 +64,7 @@ def timeCount(func):
     total_time = end_time-start_time
     print "TIME: %f\n" % total_time
 
-print "BY HAND\nLOOP 1000 to 1"
+print "BY HAND\nLOOP %d to 1" % MAX
 timeCount(solutionByHand)
 print "USING FERMAT LOOP ONLY PRIMES"
 timeCount(solutionPrimes)
