f54842b13ac9f911dc451ab16570686057e1a2a3
Jakub Ronkiewicz <jronek3010@gmail.com>
2017-08-23 00:54:09 +0200

    Improved algorithm

--- Python/032.py
+++ Python/032.py
@@ -1,16 +1,19 @@
+#21 s
+def isPandigital(n):
+    for i in range(1,10):
+        if str(i) not in n:
+            return False
+    return True
 suma = set()
 for x in range(1,1000):
     for y in range(1,10000):
         product = x*y
         string = str(x)+str(y)+str(product)
-        lst = [int(i) for i in string]
-        if 0 in lst:
-            continue
-        l = len(lst)
-        if l>9:
+        lenght = len(string)
+        if lenght>9:
             break
-        if l==9:
-            if l == len(set(lst)):
+        if lenght == 9:
+            if isPandigital(string):
                 suma.add(product)
 
 print sum(suma)
